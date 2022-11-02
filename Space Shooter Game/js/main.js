a472226de2c7fbcad4986adf58fb03c86341a905
JaysonKidoCode <99633158+JaysonKidoCode@users.noreply.github.com>
2022-11-02 14:01:26 +0800

    Add files via upload

---
+++ Space Shooter Game/js/main.js
@@ -0,0 +1,8 @@
+var SpaceHipster = SpaceHipster || {};
+
+SpaceHipster.game = new Phaser.Game('100%', '100%', Phaser.AUTO);
+
+SpaceHipster.game.state.add('GameState', SpaceHipster.GameState);
+SpaceHipster.game.state.start('GameState')
+
+console.log('Welcome to Space Shooter!')
