930144eef49c7a5c13de97b34b6887de44078000
JaysonKidoCode <99633158+JaysonKidoCode@users.noreply.github.com>
2022-11-14 18:02:59 +0800

    Add files via upload

--- Space Shooter Game/js/prefabs/EnemyBullet.js
+++ Space Shooter Game/js/prefabs/EnemyBullet.js
@@ -1,12 +1,14 @@
 var SpaceHipster = SpaceHipster || {};
 
 SpaceHipster.EnemyBullet = function(game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'bullet');
-
-    this.anchor.setTo(0.5);
-    this.checkWorldBounds = true;
-    this.outOfBoundsKill = true;
+  Phaser.Sprite.call(this, game, x, y, 'bullet'); // create a sprite using predefined key
+  
+  this.anchor.setTo(0.5);
+  
+  //Check which sprites are dead
+  this.checkWorldBounds = true;
+  this.outOfBoundsKill = true;
 };
 
-SpaceHipster.EnemyBullet.prototype = Object.create(Phaser.Sprite.prototype);
-SpaceHipster.EnemyBullet.prototype.constructer = SpaceHipster.EnemyBullet;
+SpaceHipster.EnemyBullet.prototype = Object.create(Phaser.Sprite.prototype); //inherit from Sprite class
+SpaceHipster.EnemyBullet.prototype.constructor = SpaceHipster.EnemyBullet; //note which method is the constructor
